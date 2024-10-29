3ea94471c7a39bdc251ffb490fa61ba3d6b89a58
sivanandhinisellamuthu <115562988+sivanandhinisellamuthu@users.noreply.github.com>
2024-10-29 22:33:57 +0530

    Update Problem1.java

--- Day1/Problem1.java
+++ Day1/Problem1.java
@@ -1,1 +1,20 @@
-
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int w1 = word1.length();
+        int w2 = word2.length();
+        StringBuilder res= new StringBuilder();
+        int maxlength = Math.max(w1,w2);
+        for(int i =0 ; i<maxlength ; i++)
+        {
+            if(i<w1)
+            {
+                res.append(word1.charAt(i));
+            }
+            if(i<w2)
+            {
+                res.append(word2.charAt(i));
+            }
+        } 
+        return res.toString();
+    }
+}
